fec32219c7b456cbf474e52e2e739392e8407213
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-04-13 16:03:24 +0300

    Add files via upload

--- HPCCG/x86_64/script.sh
+++ HPCCG/x86_64/script.sh
@@ -2,11 +2,13 @@
 executable="test_HPCCG"
 rm -f run-commands.sh
 prefix=$1
-for np in $(seq 1 6); do
+for np in $(seq 1 5); do
 	np_val=$((2 ** np))
-	nx=4
+	for size in $(seq 5 8); do
+		nx=$((2**size))
+		ny=$((2**size))
+		nz=$((2**size))
+		echo "mpirun --oversubscribe -np $np_val $executable $nx $ny $nz > ${prefix}_${np_val}_${nx}_${ny}_${nz}" >> run-commands.sh
+	done
 	# we write the patterns we want using the seq command
-	ny=4
-	nz=4
-	echo "mpirun --oversubscribe -np $np_val $executable $nx $ny $nz > ${prefix}_${np_val}_${nx}_${ny}_${nz}" >> run-commands.sh
 done 
