fcad36d221ac03aaa8a465d99c7346af7b6b6a9b
pi <pi@mpanana3.csd.auth.gr>
2024-04-13 15:12:05 +0100

    Made it run for ARM, with not too high problem sizes

---
+++ HPCCG/arm64/run-commands.sh
@@ -0,0 +1,15 @@
+mpirun --oversubscribe -np 2 test_HPCCG 8 8 8 > strongOut_2_8_8_8
+mpirun --oversubscribe -np 2 test_HPCCG 16 16 16 > strongOut_2_16_16_16
+mpirun --oversubscribe -np 2 test_HPCCG 32 32 32 > strongOut_2_32_32_32
+mpirun --oversubscribe -np 2 test_HPCCG 64 64 64 > strongOut_2_64_64_64
+mpirun --oversubscribe -np 2 test_HPCCG 128 128 128 > strongOut_2_128_128_128
+mpirun --oversubscribe -np 4 test_HPCCG 8 8 8 > strongOut_4_8_8_8
+mpirun --oversubscribe -np 4 test_HPCCG 16 16 16 > strongOut_4_16_16_16
+mpirun --oversubscribe -np 4 test_HPCCG 32 32 32 > strongOut_4_32_32_32
+mpirun --oversubscribe -np 4 test_HPCCG 64 64 64 > strongOut_4_64_64_64
+mpirun --oversubscribe -np 4 test_HPCCG 128 128 128 > strongOut_4_128_128_128
+mpirun --oversubscribe -np 8 test_HPCCG 8 8 8 > strongOut_8_8_8_8
+mpirun --oversubscribe -np 8 test_HPCCG 16 16 16 > strongOut_8_16_16_16
+mpirun --oversubscribe -np 8 test_HPCCG 32 32 32 > strongOut_8_32_32_32
+mpirun --oversubscribe -np 8 test_HPCCG 64 64 64 > strongOut_8_64_64_64
+mpirun --oversubscribe -np 8 test_HPCCG 128 128 128 > strongOut_8_128_128_128
---
+++ HPCCG/arm64/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+executable="test_HPCCG"
+rm -f run-commands.sh
+prefix=$1
+for np in $(seq 1 3); do
+	np_val=$((2 ** np))
+	for size in $(seq 3 7); do
+		nx=$((2**size))
+		ny=$((2**size))
+		nz=$((2**size))
+		echo "mpirun --oversubscribe -np $np_val $executable $nx $ny $nz > ${prefix}_${np_val}_${nx}_${ny}_${nz}" >> run-commands.sh
+	done
+	# we write the patterns we want using the seq command
+done 
